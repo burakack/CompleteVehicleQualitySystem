8e84227709afc45bc0728c69249b331e2d6397ba
burakack <burak_ack@hotmail.com>
2023-01-31 18:19:23 +0300

    requested changes

--- AuthService/src/main/java/com/BurakAciker/AuthService/config/ApplicationConfig.java
+++ AuthService/src/main/java/com/BurakAciker/AuthService/config/ApplicationConfig.java
@@ -1,14 +1,17 @@
 package com.BurakAciker.AuthService.config;
 
 import com.BurakAciker.AuthService.dao.AppUserRepository;
+import com.BurakAciker.AuthService.dao.RoleRepository;
+import com.BurakAciker.AuthService.domain.Role;
+import com.BurakAciker.AuthService.service.RoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private final AppUserRepository userRepository;
+    private final RoleService roleService;
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByUsername(username);
@@ -35,5 +39,25 @@ public class ApplicationConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(){
+        return args -> {
+            try {
+                roleService.createRole(new Role("ADMIN"));
+            } catch (Exception e) {
+                System.out.println("ADMIN Role already exists");
+            }
+            try {
+                roleService.createRole(new Role("TEAMLEAD"));
+            } catch (Exception e) {
+                System.out.println("TEAMLEAD Role already exists");
+            }
+            try {
+                roleService.createRole(new Role("OPERATOR"));
+            } catch (Exception e) {
+                System.out.println("OPERATOR Role already exists");
+            }
+        };
     }
 }
--- UserManagementService/src/main/java/com/BurakAciker/UserManagementService/config/ApplicationConfig.java
+++ UserManagementService/src/main/java/com/BurakAciker/UserManagementService/config/ApplicationConfig.java
@@ -1,15 +1,35 @@
 package com.BurakAciker.UserManagementService.config;
 
 
+import com.BurakAciker.UserManagementService.dto.RegisterRequest;
+import com.BurakAciker.UserManagementService.service.AppUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final AppUserService appUserService;
+
     @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    CommandLineRunner commandLineRunner(){
+        return args -> {
+
+            try{
+                appUserService.register(new RegisterRequest("adminname", "adminsurname", "toyota",
+                        "123toyota123", "toyota@gmail.com"));
+                System.out.println("ADMIN User created");
+            }catch (Exception e) {
+
+                System.out.println(e.getMessage());
+            }
+        };
     }
 }
